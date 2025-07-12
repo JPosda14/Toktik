73fb46bc76219fd0b67d9283883a2f102d845dde
Juan Andres Posada <jposada1026@cue.edu.co>
2025-07-12 11:42:23 -0500

    third commit

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infraestructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+
+  //TODO: Repository, DataSource
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future <void> loadNextPage () async {
-    //todo : cargar los videos
 
+    await Future.delayed( const Duration(seconds: 2));
+
+
+      final List<VideoPost> newVideos = videoPosts.map(
+      (video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity()
+      ).toList();
+
+      
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   } 
 
