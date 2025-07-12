780f42a979d731f373e41689f7dd9d8e05061f2c
Juan Andres Posada <jposada1026@cue.edu.co>
2025-07-12 12:16:18 -0500

    cuarto commit

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Discover Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
+
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+
+
+
+
+    return Scaffold(
+     body: discoverProvider.
+      initialLoading 
+          ? const Center ( child : CircularProgressIndicator(strokeWidth: 2,))
+          : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
